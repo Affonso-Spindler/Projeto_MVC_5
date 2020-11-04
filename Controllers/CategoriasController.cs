36a901ad5d3091011b13d4de1ffef31cbdcfe512
Affonso_Spindler <affonso_spindler@hotmail.com>
2020-11-04 09:22:31 -0300

    Implementanto a Interação da Action com a Visão

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto_MVC_5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,32 @@ namespace Projeto_MVC_5.Controllers
 {
     public class CategoriasController : Controller
     {
+
+        private static IList<Categoria> categorias =
+            new List<Categoria>()
+            {
+                new Categoria() {
+                    CategoriaId = 1,
+                    Nome =   "Notebooks"},
+                new Categoria() {
+                    CategoriaId = 2,
+                    Nome =   "Monitores"},
+                new Categoria() {
+                    CategoriaId = 3,
+                    Nome =   "Impressoras"},
+                new Categoria() {
+                    CategoriaId = 4,
+                    Nome =   "Mouses"},
+                new Categoria() {
+                    CategoriaId = 5,
+                    Nome =   "Desktops"}
+            };
+
+
         // GET: Categorias
         public ActionResult Index()
         {
-            return View();
+            return View(categorias);
         }
     }
 }
