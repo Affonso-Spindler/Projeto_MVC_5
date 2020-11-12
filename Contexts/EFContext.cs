74d3ef91b4b6771d50e0f13ce225548bfd9d2f32
Affonso_Spindler <affonso_spindler@hotmail.com>
2020-11-12 08:56:59 -0300

    Começando com o Entity Framework

---
+++ Contexts/EFContext.cs
@@ -0,0 +1,18 @@
+﻿using Projeto_MVC_5.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Projeto_MVC_5.Contexts
+{
+	public class EFContext : DbContext
+	{
+		public EFContext() : base("Asp_Net_MVC_CS") { }
+		public DbSet<Categoria> Categorias { get; set; }
+		public DbSet<Fabricante> Fabricantes
+		{ get; set; }
+	}
+
+}
